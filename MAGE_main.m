814db3b7fd064d97576ef8136199b102adb51e5d
beltranmm <107505294+beltranmm@users.noreply.github.com>
2024-03-01 18:19:07 -0500

    remove validation figures from MAGE

--- MAGE_main.m
+++ MAGE_main.m
@@ -4,7 +4,13 @@
 % load MAGE functions
 f = MAGE_functions;
 
-profile = f.simData(100,4,7,1,0.1);
+profile = f.simData(1000,4,7,1,0.1);
 
 figure;
 scatter(mean(profile(:,1:4),2),mean(profile(:,5:8),2))
+
+% filter code here
+
+%% run MAGE
+
+[~,OS] = MAGE(profile(:,1:4),profile(:,5:8),100,5,1);
